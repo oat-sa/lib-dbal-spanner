787a7bec61476ebef812949d482ccde7da649979
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2020-10-14 14:19:14 +0200

    return the static databases if they are set

--- src/SpannerDriver.php
+++ src/SpannerDriver.php
@@ -45,6 +45,7 @@ class SpannerDriver implements Driver
     public const DRIVER_OPTION_CREDENTIALS_FETCHER = 'driver-option-credentials-fetcher';
     public const DRIVER_OPTION_CLIENT_CONFIGURATION = 'driver-option-client-configuration';
     public const DRIVER_OPTION_CREDENTIALS_FILE_PATH = 'driver-option-credentials-file-path';
+    public const DRIVER_OPTION_DATABASES = 'driver-option-databases';
 
     private const SESSIONS_MIN = 1;
     private const SESSIONS_MAX = 100;
@@ -212,6 +213,10 @@ class SpannerDriver implements Driver
      */
     public function listDatabases(string $instanceName = ''): array
     {
+        if (!empty($this->driverOptions[self::DRIVER_OPTION_DATABASES])) {
+            return $this->driverOptions[self::DRIVER_OPTION_DATABASES];
+        }
+
         if ($instanceName === '') {
             $instanceName = $this->instanceName;
         }
